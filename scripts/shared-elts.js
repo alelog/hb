23932cc5fcd3f2619968848b459aa04833f68526
Alexey Loginov <alelog@gmail.com>
2019-08-18 19:35:38 -0400

    Factor out shared-element handling

---
+++ scripts/shared-elts.js
@@ -0,0 +1,8 @@
+var placeholders = ['#header', '#topnav', '#footer'];
+$(function() {  // Call this from DOM's .ready()
+    for (var i = 0; i < placeholders.length; i++) {
+        // Replace placeholders on this page with matching shared
+        // elements in load.html
+        $(placeholders[i]).load('load.html ' + placeholders[i] + '-shared');
+    }
+});
